e959a4c9161a7cd3c5d47b0d5a7955af94e3d64e
Akira Komamura <akira.komamura@gmail.com>
2022-03-24 14:21:18 +0900

    feat: Improve error reporting
    Make the error message less noisy and report the summary as the final message.

--- github-linguist.el
+++ github-linguist.el
@@ -49,6 +49,8 @@
   "Analyse projects using GitHub Linguist."
   :group 'project)
 
+(defconst github-linguist-error-buffer "*github-linguist errors*")
+
 (defconst github-linguist-git-dir ".git"
   "Name of the git directory.")
 
@@ -169,41 +171,52 @@ If ARG is non-nil, existing projects are updated as well."
    `(lambda ()
       (load ,github-linguist-library nil t)
       (let ((queue ',directories)
+            (error-file (make-temp-file "emacs-gh-linguist-errors" nil ".txt"))
+            (error-buffer (generate-new-buffer "*github-linguist errors*"))
             process-errors
             parse-errors
             directory)
-        (while (setq directory (pop queue))
-          (with-temp-buffer
-            (if (zerop (call-process ,github-linguist-executable
-                                     nil
-                                     (list (current-buffer) nil)
-                                     nil
-                                     (github-linguist--system-file-name directory)
-                                     "--json"))
-                (condition-case nil
-                    (thread-last (github-linguist--parse-buffer)
-                      (github-linguist--update directory))
-                  (error (push directory parse-errors)))
-              (push directory process-errors))))
+        (unwind-protect
+            (while (setq directory (pop queue))
+              (with-temp-buffer
+                (if (zerop (call-process ,github-linguist-executable
+                                         nil
+                                         (list (current-buffer) error-file)
+                                         nil
+                                         (github-linguist--system-file-name directory)
+                                         "--json"))
+                    (condition-case nil
+                        (thread-last (github-linguist--parse-buffer)
+                                     (github-linguist--update directory))
+                      (error (push directory parse-errors)))
+                  (with-current-buffer error-buffer
+                    (insert-file-contents error-file))
+                  (push directory process-errors))))
+          (delete-file error-file))
         (list github-linguist-results
-              :parse-errors parse-errors
-              :process-errors process-errors)))
+              :error-string (when process-errors
+                              (with-current-buffer error-buffer
+                                (buffer-string)))
+              :parse-errors parse-errors)))
    (pcase-lambda (`(,hashtable . ,plist))
      (github-linguist--ensure-table)
      (map-do (lambda (key value)
                (puthash key value github-linguist-results))
              hashtable)
      (github-linguist--save)
-     (message "Updated %d linguist projects (in %.1f sec)"
-              (map-length hashtable)
-              (- (float-time) github-linguist-start-time))
-     (setq github-linguist-start-time nil)
-     (when-let (process-errors (plist-get plist :process-errors))
-       (message "Linguist failed on the following projects: %s"
-                process-errors))
-     (when-let (parse-errors (plist-get plist :parse-errors))
-       (message "Parsing failed on the results from the following projects: %s"
-                parse-errors)))))
+     (let* ((has-error (with-current-buffer (get-buffer-create github-linguist-error-buffer)
+                         (insert (or (plist-get plist :error-string) ""))
+                         (not (zerop (buffer-size)))))
+            (parse-errors (plist-get plist :parse-errors)))
+       (when parse-errors
+         (error "Failed to parse output of GitHub Linguist on some projects"))
+       (message "Updated %d linguist projects (in %.1f sec%s)"
+                (map-length hashtable)
+                (- (float-time) github-linguist-start-time)
+                (if has-error
+                    (format ", see %s on errors" github-linguist-error-buffer)
+                  "")))
+     (setq github-linguist-start-time nil))))
 
 ;;;###autoload
 (defun github-linguist-run (directory &optional callback)
